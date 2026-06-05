d6c94f8d9f3b8adb1ebe7f8d9507c4505781234a
Pedro Ribeiro <112722390+pedro-hgr@users.noreply.github.com>
2026-06-05 06:59:03 -0300

    dockerfile

--- front/Prototipo Front-end APP/backend/src/main/java/com/example/bateponto/WebConfig.java
+++ front/Prototipo Front-end APP/backend/src/main/java/com/example/bateponto/WebConfig.java
@@ -12,7 +12,6 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:5173", "http://127.0.0.1:5173","*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedHeaders("*");
     }
 }
